8849af28d24131723ec51d9b3daed93cbfb196aa
Sridatthu <134670326+Sridatthu@users.noreply.github.com>
2025-11-11 17:33:44 +0530

    added toggle-mode

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,10 @@
+import { ModeToggle } from "@/components/mode-toggle";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+    <div className="relative flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
     Check Branches for the Code 
+    <ModeToggle/>
     </div>
   );
 }
