6e30221e72244913faac04048f5c3cebe1835021
Sridatthu <134670326+Sridatthu@users.noreply.github.com>
2025-11-12 11:08:40 +0530

    svg animations

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,11 @@
 import { ModeToggle } from "@/components/mode-toggle";
+import { SvgComponent } from "@/components/svg-component";
+import { SvgLines } from "@/components/SvgLines";
 
 export default function Home() {
   return (
-    <div className="relative flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-    Check Branches for the Code 
-    <ModeToggle/>
+    <div className="max-w-4xl mx-auto flex h-screen items-center flex-col bg-gray-50 py-20 ">
+ <SvgLines />
     </div>
   );
 }
